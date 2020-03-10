ad9c05d759e08d9cc1c670301b231808259dea60
Jordi Prous <jprous85@gmail.com>
2020-03-10 09:08:35 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(()=>{
-    let toastr = $(document).toastr({
-        align: 'right',
-        status: 'notice',
-        text_heading: 'SUCCESS',
-        text_body: 'Lorem ipsum'
-    });
-});
