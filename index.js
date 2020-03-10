f30e4626e20a71a9d9ea610a2b0efbe381a4d29a
jprous85 <jprous85@gmail.com>
2020-03-10 08:57:37 +0100

    toastr project

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+    let toastr = $(document).toastr({
+        align: 'right',
+        status: 'notice',
+        text_heading: 'SUCCESS',
+        text_body: 'Lorem ipsum'
+    });
+});
